b9edf368e0f4a13315fe02f517ff4154f0ebdbc3
marc1107 <m.bohner99@gmail.com>
2022-11-21 16:12:53 +0100

    Aufgabe 3 fertig (ohne normieren)

--- Versuch2/versuch2.py
+++ Versuch2/versuch2.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 
 image = cv2.imread('./Bilder/Grauwertkeil.png')
-image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+grauwertkeil = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 # Aufbau der beiden Arrays: alle ungeraden Werte sind Startwerte eines Bereichs,
 # die geraden die zugehörigen Endwerte
@@ -15,25 +15,25 @@ bottomRow.append(5)
 
 # topRow füllen
 i = 80
-while i < len(image[0]):
-    if i + 10 < len(image[0]):
-        if int(image[0][i + 10]) - int(image[0][i]) >= 20:
+while i < len(grauwertkeil[0]):
+    if i + 10 < len(grauwertkeil[0]):
+        if int(grauwertkeil[0][i + 10]) - int(grauwertkeil[0][i]) >= 20:
             topRow.append(i)  # Endwert speichern
             i += 15
             topRow.append(i)  # neuen Startwert speichern
     i += 1
-topRow.append(len(image[0]) - 5)
+topRow.append(len(grauwertkeil[0]) - 5)
 
 # bottomRow füllen
 i = 0
-while i < len(image[len(image) - 1]):
-    if i + 10 < len(image[len(image) - 1]):
-        if int(image[len(image) - 1][i + 10]) - int(image[len(image) - 1][i]) >= 20:
+while i < len(grauwertkeil[len(grauwertkeil) - 1]):
+    if i + 10 < len(grauwertkeil[len(grauwertkeil) - 1]):
+        if int(grauwertkeil[len(grauwertkeil) - 1][i + 10]) - int(grauwertkeil[len(grauwertkeil) - 1][i]) >= 20:
             bottomRow.append(i)  # Endwert speichern
             i += 15
             bottomRow.append(i)  # neuen Startwert speichern
     i += 1
-bottomRow.append(len(image[len(image) - 1]) - 5)
+bottomRow.append(len(grauwertkeil[len(grauwertkeil) - 1]) - 5)
 
 xValues = []
 for i in range(len(topRow)):
@@ -48,12 +48,12 @@ for i in range(len(topRow)):
 
 ROI_number = 0
 yTop = 5
-yBottom = len(image) - 5
+yBottom = len(grauwertkeil) - 5
 subPicsMean = []
 subPicsStd = []
 i = 0
 while i < len(xValues):
-    ROI = image[yTop:yBottom, xValues[i]:xValues[i + 1]]
+    ROI = grauwertkeil[yTop:yBottom, xValues[i]:xValues[i + 1]]
     subPicsMean.append(np.mean(ROI))
     subPicsStd.append(np.std(ROI, ddof=1))
     cv2.imwrite('SubPic_{}.png'.format(ROI_number), ROI)
@@ -63,18 +63,33 @@ while i < len(xValues):
 print("Mittelwerte SubPics:\n{}".format(subPicsMean))
 print("Standardabweichung SubPics:\n{}".format(subPicsStd))
 
-# for i in range(len(image[0])):
-#     print("Pixel {}: {}".format(i, image[0][i]))
+# for i in range(len(grauwertkeil[0])):
+#     print("Pixel {}: {}".format(i, grauwertkeil[0][i]))
 
-# print("X-Wert: " + str(len(image[0])))
-# print("Y-Wert: " + str(len(image)))
+# print("X-Wert: " + str(len(grauwertkeil[0])))
+# print("Y-Wert: " + str(len(grauwertkeil)))
 
-# cv2.imshow('image', image)
+# cv2.imshow('grauwertkeil', grauwertkeil)
 # cv2.waitKey(0)
 
+def bildKontrastMaximiert(img, filename):
+    imgCorrected = img
+    min = np.min(imgCorrected)
+    max = np.max(imgCorrected)
+    for i in range(480):
+        for j in range(640):
+            imgCorrected[i][j] = imgCorrected[i][j] - min
+            if max > min:
+                imgCorrected[i][j] = imgCorrected[i][j] * (255 / (max - min))
+            else:
+                imgCorrected[i][j] = imgCorrected[i][j] * 255
+
+    cv2.imwrite('{}.png'.format(filename), imgCorrected)
+    print("{}.png gespeichert".format(filename))
+    return imgCorrected
 
 # ---------------------------------------------------------
-# Teil2
+# Teil 2
 # ---------------------------------------------------------
 darkImagesGray = []
 for i in range(10):
@@ -82,7 +97,7 @@ for i in range(10):
     darkImagesGray.append(cv2.cvtColor(image, cv2.COLOR_BGR2GRAY))
 
 
-def pixelwiseMean(arr):
+def pixelwiseMeanAndSave(arr, filename):
     imageMean = np.zeros(shape=(len(arr[0]), len(arr[0][0])))
 
     for i in range(len(arr[0])):
@@ -93,13 +108,70 @@ def pixelwiseMean(arr):
                 values.append(arr[k][i][j])
             line.append(np.mean(values))
         imageMean[i] = line
+    cv2.imwrite('{}.png'.format(filename), imageMean)
+    print("{}.png gespeichert".format(filename))
 
     return imageMean
 
 
-darkImageMean = pixelwiseMean(darkImagesGray)
-cv2.imwrite('Dunkelbild.png', darkImageMean)
+darkImageMean = pixelwiseMeanAndSave(darkImagesGray, "Dunkelbild")
+bildKontrastMaximiert(darkImageMean, "Dunkelbild_kontrastmax")
 
-print("DarkImageMean Array:\n{}".format(darkImageMean))
+# print("DarkImageMean Array:\n{}".format(darkImageMean))
 # print("DarkImage Y: {}".format(len(darkImageMean)))
 # print("DarkImage X: {}".format(len(darkImageMean[0])))
+
+def subtractAndSaveAs(img, darkImg, filename):
+    subImg = np.zeros(shape=(len(img), len(img[0])))
+
+    for i in range(len(img)):
+        for j in range(len(img[0])):
+            subImg[i][j] = img[i][j] - darkImg[i][j]
+
+    cv2.imwrite('{}.png'.format(filename), subImg)
+    print("{}.png gespeichert".format(filename))
+    return subImg
+
+
+# Dunkelbild von Grauwertkeil subtrahieren und speichern
+subtractAndSaveAs(grauwertkeil, darkImageMean, "NoBildrauschen")
+
+# ---------------------------------------------------------
+# Teil 3
+# ---------------------------------------------------------
+# Mittelwertbild von 10 Weißbildern
+whiteImagesGray = []
+for i in range(10):
+    image = cv2.imread('./Bilder/Weissbild ({}).png'.format(i + 1))
+    whiteImagesGray.append(cv2.cvtColor(image, cv2.COLOR_BGR2GRAY))
+
+whiteImageMean = pixelwiseMeanAndSave(whiteImagesGray, "Weissbild")
+whiteImageMean = subtractAndSaveAs(whiteImageMean, darkImageMean, "Weissbild")
+bildKontrastMaximiert(whiteImageMean, "Weißbildbild_kontrastmax")
+
+
+def weissbildNormieren(img):
+    return img
+
+
+def divideAndSaveAs(img, whiteImg, filename):
+    divImg = np.zeros(shape=(len(img), len(img[0])))
+
+    for i in range(len(img)):
+        for j in range(len(img[0])):
+            if whiteImg[i][j] > 0:
+                divImg[i][j] = float(img[i][j]) / float(whiteImg[i][j])
+            else:
+                print("Deadpixel x/y: {}/{}".format(j, i))
+                divImg[i][j] = float(img[i][j])
+
+    cv2.imwrite('{}.png'.format(filename), divImg)
+    print("{}.png gespeichert".format(filename))
+
+
+whiteImageNormiert = weissbildNormieren(whiteImageMean)
+divideAndSaveAs(grauwertkeil, whiteImageNormiert, "Dividiert")
+
+# ---------------------------------------------------------
+# Teil 4
+# ---------------------------------------------------------
